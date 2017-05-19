c8e91eb936c6acb7759e820184911e62690dc9d4
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2017-05-19 16:13:46 +0200

    gestart met maken van ebook parser.

---
+++ Utilities/EbookParser/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Windows.Forms;
+
+namespace EbookParser
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            //selecting files
+            OpenFileDialog fileselecter = new OpenFileDialog();
+            fileselecter.Multiselect = true;
+            fileselecter.ShowDialog();
+            List<string> allText = new List<string>();
+
+            foreach (var file in fileselecter.FileNames)
+            {
+                var xml = XDocument.Load(file);
+                var df = xml.Root.Name.Namespace;
+                
+                var pElemenets = xml.Root.Descendants(df + "p");
+                foreach(var i in pElemenets)
+                {
+                    allText.Add(CleanToText(i));
+                }
+            }
+
+        }
+
+        static string CleanToText(XElement element)
+        {
+            foreach(var i in element.Elements())
+            {
+                i.ReplaceWith(CleanToText(i));
+            }
+            return element.Value;
+        }
+    }
+}
